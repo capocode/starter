efe7f7a89a6a70d42d9ee4ae0724402dcf61abbb
Anthony Holmes <anthony@antlur.co>
2020-11-24 01:11:57 -0600

    add laravel mix and tailwind

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+  // plugins: {
+  //   "postcss-import": {},
+  //   tailwindcss: {},
+  //   autoprefixer: {},
+  // }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/components/**/*.php',
+    './src/pages/**/*.php'
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
