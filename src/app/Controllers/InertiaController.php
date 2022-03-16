7fb52f55e748b63511f7b33c38fdf936623e82f7
Anthony Holmes <anthony@antlur.co>
2022-03-15 23:55:09 -0500

    Inertia page

--- src/app/Controllers/InertiaController.php
+++ src/app/Controllers/InertiaController.php
@@ -3,22 +3,11 @@
 namespace App\Controllers;
 
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
 
 class InertiaController extends Controller
 {
     public function index()
     {
-        if (!Cache::has('locations')) {
-            $res = Http::get('https://bernie.docbs.com/api/locations');
-            Cache::add('locations', $res->json(), now()->addSeconds(30));
-        }
-
-        $locations = Cache::get('locations');
-
-        return inertia('inertia', [
-            'locations' => $locations,
-        ]);
+        return inertia('inertia');
     }
 }
