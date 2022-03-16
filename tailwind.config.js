90c8504839f8055bd8596b5f090f8dba991ecdb0
Anthony Holmes <anthony@antlur.co>
2022-03-15 23:55:23 -0500

    upgrade tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  purge: [
+  content: [
     "./src/**/*.{php,js,jsx,ts,tsx}",
   ],
-  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
