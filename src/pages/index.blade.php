5b7e2af58e006690a0bceb0d183b2e49d6622c01
Anthony Holmes <anthony@antlur.co>
2022-03-17 00:36:37 -0500

    remove blade pages

--- src/pages/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app>
-    <div class="p-12">
-        <x-component />
-        <ul class="list-disc list-inside">
-            <li><a href="/inertia">Inertia Page</a></li>
-            <li><a href="/about">Inertia About</a></li>
-        </ul>
-    </div>
-</x-app>
