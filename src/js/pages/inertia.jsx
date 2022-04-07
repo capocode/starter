bae253077d2dead65dbc5908f81c382de321d812
Anthony Holmes <anthony@antlur.co>
2022-04-06 19:45:24 -0500

    Inertia view

--- src/js/pages/inertia.jsx
+++ src/js/pages/inertia.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Link } from "@inertiajs/inertia-react";
 
 export default function Inertia(props) {
   return (
     <div>
       <h1>Inertia</h1>
-      <p>
-        <Link href="/about">About</Link>
-      </p>
+      <p>Hello World</p>
     </div>
   );
 }
