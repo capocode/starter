c749e277d96b8d65fb5153755962c248dc297d6b
Anthony Holmes <anthony@antlur.co>
2022-04-06 19:45:35 -0500

    Inertia route

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Controllers\InertiaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [InertiaController::class, 'index']);
-Route::inertia('about', 'about');
+Route::inertia('/', 'inertia');
