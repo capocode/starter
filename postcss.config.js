a7ba714cd9b191444063e06bd2b7e90ce83b5d92
Anthony Holmes <anthony@antlur.co>
2023-07-31 02:02:32 -0500

    simplify frontend

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-  // plugins: {
-  //   "postcss-import": {},
-  //   tailwindcss: {},
-  //   autoprefixer: {},
-  // }
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
